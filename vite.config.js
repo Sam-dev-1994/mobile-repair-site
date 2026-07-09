7511a59bdc40ee63dbbd998300130e6234c9d103
Masoumeh Hassani <m.hassany128@gmail.com>
2026-07-09 15:49:49 +0330

    fix github pages base

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel';
-import tailwindcss from '@tailwindcss/vite'
+// import babel from '@rolldown/plugin-babel';
+// import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
-    babel({ presets: [reactCompilerPreset()] }),
-
+    // tailwindcss(),
+    // babel({ presets: [reactCompilerPreset()] }),
   ],
-  base:"/mobile-repair-site/"
+  base: "/mobile-repair-site/"
 })
